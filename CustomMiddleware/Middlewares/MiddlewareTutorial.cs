77da9fb7f671e3c0719d02cbe2af9984c404f711
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-09-26 09:39:35 +0700

    Add project files.

---
+++ CustomMiddleware/Middlewares/MiddlewareTutorial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CustomMiddleware.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MiddlewareTutorial
+    {
+        private readonly RequestDelegate _next;
+
+        public MiddlewareTutorial(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await httpContext.Response.WriteAsync("Hello Readers, this is from Custom Middleware...");
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareTutorialExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareTutorial(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MiddlewareTutorial>();
+        }
+    }
+}
---
+++ CustomMiddleware/Program.cs
@@ -0,0 +1,12 @@
+using CustomMiddleware.Middlewares;
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMiddlewareTutorial();
+}
+app.Run();
